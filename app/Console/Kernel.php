c057bf789aa89db1c95d8f722e3360aeea9794e6
Gavin Kimpson <gkimpson@gmail.com>
2022-02-17 08:21:07 +0000

    add commands to get match data to scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('proclubsapi:matches')->everyFifteenMinutes()->description('Get latest matches data');
     }
 
     /**
