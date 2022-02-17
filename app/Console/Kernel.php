b22753825e269d0931dc0820fb8d62c9745360e9
Gavin Kimpson <gkimpson@gmail.com>
2022-02-17 08:21:39 +0000

    remove some pointless line of code

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
         $schedule->command('proclubsapi:matches')->everyFifteenMinutes()->description('Get latest matches data');
     }
 
