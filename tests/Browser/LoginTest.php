c72f95811fd5ca0e02d6194d15554890c89790e0
Gavin Kimpson <gkimpson@gmail.com>
2022-03-13 11:58:54 +0000

    login test works

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -5,6 +5,7 @@ namespace Tests\Browser;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
+use stdClass;
 use Tests\DuskTestCase;
 
 class LoginTest extends DuskTestCase
@@ -16,20 +17,20 @@ class LoginTest extends DuskTestCase
      *
      * @return void
      */
-    public function test_basic_example()
+    public function test_login()
     {
-        // $user = User::factory()->create([
-        //     'email' => 'gkimpson@gmail.com',
-        // ]);
-
-        $user = [];
+        $user = [
+            'email' => 'gkimpson@gmail.com',
+            'password' => 'password'
+        ];
+        
  
         $this->browse(function ($browser) use ($user) {
             $browser->visit('/login')
-                    ->type('email', 'gkimpson@gmail.com')
-                    ->type('password', 'password')
-                    ->press('Login')
-                    ->assertPathIs('/home');
+                    ->type('email', $user['email'])
+                    ->type('password', $user['password'])
+                    ->click('@login-button')
+                    ->assertPathIs('/dashboard');
         });
     }
 }
