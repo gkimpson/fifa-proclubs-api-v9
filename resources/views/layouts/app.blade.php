819fb005700f4acc464429dd1c3927d7dcf4018a
Gavin Kimpson <gkimpson@gmail.com>
2022-05-27 08:41:23 +0100

    added fix for bladewind datepicker component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'ProClubsAPI') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
+        <link href="{{ asset('bladewind/css/animate.min.css') }}" rel="stylesheet" />
+        <link href="{{ asset('bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="{{ asset('css/lity.min.css') }}">
 
+
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
-
         <script src="{{ asset('js/jquery-2.2.4.min.js') }}" defer></script>
         <script src="{{ asset('js/lity.min.js') }}" defer></script>
-
+        <script>var notification_timeout,user_function,el_name,momo_obj,delete_obj;var dropdownIsOpen = false;</script>
         @stack('head-scripts')
     </head>
     <body class="font-sans antialiased">
@@ -36,14 +37,18 @@
 
             <!-- Page Content -->
             <main>
+                <x-bladewind.datepicker
+                    css="!w-40"
+                    placeholder="Invoice Date"  />
+
                 {{ $slot }}
             </main>
         </div>
-        <script src="https://cdn.zipy.ai/sdk/v1.0/zipy.min.umd.js" crossorigin="anonymous"></script> <script> window.zipy && window.zipy.init('e038a759');</script>
-        <script
-            type="text/javascript"
-            src="https://sdk.relicx.ai/relicx-sdk.min.js"
-            onload="relicxSDK.init({orgID: '322c76a1-eaaa-493f-9bac-758d5e87a695',appID: '304a9ad9-254b-474f-9f0c-5b8116eec0a5'})">
-        </script>
+{{--        <script src="https://cdn.zipy.ai/sdk/v1.0/zipy.min.umd.js" crossorigin="anonymous"></script> <script> window.zipy && window.zipy.init('e038a759');</script>--}}
+{{--        <script--}}
+{{--            type="text/javascript"--}}
+{{--            src="https://sdk.relicx.ai/relicx-sdk.min.js"--}}
+{{--            onload="relicxSDK.init({orgID: '322c76a1-eaaa-493f-9bac-758d5e87a695',appID: '304a9ad9-254b-474f-9f0c-5b8116eec0a5'})">--}}
+{{--        </script>--}}
     </body>
 </html>
