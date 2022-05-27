65d1343b4ce103409a0dc28f44380cbe06929ff4
Gavin Kimpson <gkimpson@gmail.com>
2022-05-27 08:50:35 +0100

    re-add tracking

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,11 +40,11 @@
                 {{ $slot }}
             </main>
         </div>
-{{--        <script src="https://cdn.zipy.ai/sdk/v1.0/zipy.min.umd.js" crossorigin="anonymous"></script> <script> window.zipy && window.zipy.init('e038a759');</script>--}}
-{{--        <script--}}
-{{--            type="text/javascript"--}}
-{{--            src="https://sdk.relicx.ai/relicx-sdk.min.js"--}}
-{{--            onload="relicxSDK.init({orgID: '322c76a1-eaaa-493f-9bac-758d5e87a695',appID: '304a9ad9-254b-474f-9f0c-5b8116eec0a5'})">--}}
-{{--        </script>--}}
+        <script src="https://cdn.zipy.ai/sdk/v1.0/zipy.min.umd.js" crossorigin="anonymous"></script> <script> window.zipy && window.zipy.init('e038a759');</script>
+        <script
+            type="text/javascript"
+            src="https://sdk.relicx.ai/relicx-sdk.min.js"
+            onload="relicxSDK.init({orgID: '322c76a1-eaaa-493f-9bac-758d5e87a695',appID: '304a9ad9-254b-474f-9f0c-5b8116eec0a5'})">
+        </script>
     </body>
 </html>
