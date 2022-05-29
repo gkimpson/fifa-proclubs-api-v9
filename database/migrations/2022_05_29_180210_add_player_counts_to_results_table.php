2f25a31a9786adef90dda8a7c8c6733124b0747b
Gavin Kimpson <gkimpson@gmail.com>
2022-05-29 19:14:00 +0100

    player counts migration on results table

---
+++ database/migrations/2022_05_29_180210_add_player_counts_to_results_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('results', function (Blueprint $table) {
+            $table->unsignedTinyInteger('home_team_player_count')->after('away_team_goals');
+            $table->unsignedTinyInteger('away_team_player_count')->after('home_team_player_count');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('results', function (Blueprint $table) {
+            $table->dropColumn('home_team_player_count');
+            $table->dropColumn('away_team_player_count');
+        });
+    }
+};
