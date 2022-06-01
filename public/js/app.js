82415a355c2d4c628c47a28355c20ab8cf055fb3
Gavin Kimpson <gkimpson@gmail.com>
2022-06-01 19:47:08 +0100

    fix styles

--- public/js/app.js
+++ public/js/app.js
@@ -5080,7 +5080,7 @@ __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
 
 window.Alpine = alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"];
-alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start(); // TODO - this line SHOULD NOT BE COMMENTED OUT - currently there is an issue with bladewind & datepicker when the line is active
+alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start();
 
 /***/ }),
 
