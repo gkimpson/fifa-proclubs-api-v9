9c3c2935b6c174ce92f22a9d12689e602f2dd260
Gavin Kimpson <gkimpson@gmail.com>
2022-06-09 16:24:24 +0100

    remove the vanity message for php :)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
                 </div>
                 <div class="flex justify-center mt-4 sm:items-center">
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        Laravel v{{ Illuminate\Foundation\Application::VERSION }}
                     </div>
                 </div>
                 <div class="flex justify-center sm:items-center">
